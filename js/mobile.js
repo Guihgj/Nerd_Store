3c31a74caf80877311b5e75bdcf2867b60a2391d
Guilherme Henrique <guihgj11@outlook.com>
2022-03-02 08:35:11 -0300

    Loja

---
+++ js/mobile.js
@@ -0,0 +1,8 @@
+<script>
+const toggleButton = document.getElementsByClassName('menu-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+navbarLinks.classList.toggle('active')
+})
+</script>
